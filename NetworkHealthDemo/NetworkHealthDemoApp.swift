c2cab9917590f114b68d26b26eb956c2d872529a
Andrey Torlopov <torlopov.mail@ya.ru>
2025-11-03 13:12:50 +0300

    Initial Commit

---
+++ NetworkHealthDemo/NetworkHealthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkHealthDemoApp.swift
+//  NetworkHealthDemo
+//
+//  Created by Andrey Torlopov on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NetworkHealthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkHealthDemoTests/NetworkHealthDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkHealthDemoTests.swift
+//  NetworkHealthDemoTests
+//
+//  Created by Andrey Torlopov on 03.11.2025.
+//
+
+import Testing
+@testable import NetworkHealthDemo
+
+struct NetworkHealthDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
